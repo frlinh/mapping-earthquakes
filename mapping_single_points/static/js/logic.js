d7e621a272342b62ce593832acae7a0d7b9582fe
frlinh <ci_linh@outlook.com>
2022-07-17 20:16:21 -0700

    adding 'mapping_single_points' dir from 'main' branch

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -11,14 +11,6 @@ L.circleMarker([34.0522, -118.2437], {
     radius: 300
 }).addTo(map);
 
-// // OR Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
